b1dc6a3065d1dea90bdd9414765b332232b4810f
ParkSangSun1 <appleasz1028102@gmail.com>
2021-07-15 10:16:09 +0900

    [CREATE] Navigation component setup

--- app/src/main/java/com/study/food/MainActivity.kt
+++ app/src/main/java/com/study/food/MainActivity.kt
@@ -2,10 +2,46 @@ package com.study.food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //navController = findNavController(R.id.navHostFragment)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment)
+        if (navHostFragment != null) {
+            navController = navHostFragment.findNavController()
+        }
+
+        //appbar 구성
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    // 바텀 네비게이션 프래그먼트와 연결해주기
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 }
